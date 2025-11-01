e577ff1c5cfd71edc7db290789ddcd2596c8e815
unknown <shteinaseev@gmail.com>
2025-11-01 13:39:20 +0100

    fgh

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 class Calculator {
-    operationList = ['(', ')', '.'];
+    operationList = [];
     stack = [];
     output = [];
 
@@ -9,7 +9,8 @@ class Calculator {
         ["*", 3],
         ["/", 3],
         ["^", 4],
-        ["(", 5]
+        ["(", 5],
+        ["%", 4]
     ])
     
     constructor(previousOperand, currentOperand, numberBTNs, operationBTNs, eqlBTN, acBTN, delBTN, pointBTN, parenthesis) {
@@ -46,8 +47,8 @@ class Calculator {
         }
 
         eqlBTN.addEventListener('click', () => {
+            this.reversePOlNotation();
             this.calculate();
-            this.reversePOlNotationConv();
         })
 
         acBTN.addEventListener('click', () => {
@@ -57,64 +58,144 @@ class Calculator {
         delBTN.addEventListener('click', () => {
             this.delSymbol();
         })
+
+        pointBTN.addEventListener('click', () => {
+            this.appendPoint();
+        })
+    }
+
+    canAddPoint(expression){ 
+        if(expression.length === 0){
+            return true;
+        }
+
+        let lastChar = expression.slice(-1);
+
+        if(this.isOperator(lastChar) || this.isParenthesis(lastChar)){
+            return false;
+        }
+
+        let currentNumber = '';
+        for (let i = expression.length - 1; i >= 0; i--) {
+            if(this.isOperator(expression[i]) || this.isParenthesis(expression[i])){
+                break;
+            }
+            currentNumber = expression[i] + currentNumber;
+        }
+        return !currentNumber.includes('.');
+
+    }
+
+    appendPoint() {
+        const expression = this.currentOperand.innerText;
+        let lastChar = expression.slice(-1);
+        if(expression.length === 0 || this.isOperator(lastChar) || this.isParenthesis(lastChar)){
+            this.currentOperand.innerText += '0.';
+            return;
+        }
+
+        if(this.canAddPoint(expression)){
+            this.currentOperand.innerText += '.';
+        }
     }
 
     calculate() {   
-        this.previousOperand.innerText += this.currentOperand.innerText 
-        // let operator = previousOperand.innerText.slice(-1);
-        // switch (operator) {
-        //     case '+':
-        //         this.currentOperand.innerText = parseFloat(this.previousOperand.innerText) + parseFloat(this.currentOperand.innerText);
-        //         this.previousOperand.innerText = '';
-        //         break;
-        //     case '-':
-        //         this.currentOperand.innerText = parseFloat(this.previousOperand.innerText) - parseFloat(this.currentOperand.innerText);
-        //         this.previousOperand.innerText = '';
-        //         break;
-        //     case '/':
-        //         this.currentOperand.innerText = parseFloat(this.previousOperand.innerText) / parseFloat(this.currentOperand.innerText);
-        //         this.previousOperand.innerText = '';
-        //         break;
-        //     case '*':
-        //         this.currentOperand.innerText = parseFloat(this.previousOperand.innerText) * parseFloat(this.currentOperand.innerText);
-        //         this.previousOperand.innerText = '';
-        //         break;
-        //     case '%':
-        //         this.currentOperand.innerText = parseFloat(this.currentOperand.innerText / 100) * parseFloat(this.previousOperand.innerText);
-        //         this.previousOperand.innerText = '';
-        //         break;
-        // }
+        this.output.forEach((i) => {
+            if(this.isOperator(i)){
+                let b = this.stack.pop()
+                let a = this.stack.pop()
+                switch(i){
+                    case '+':
+                        this.stack.push(a + b)
+                        break;
+                    case '-':
+                        this.stack.push(a - b)
+                        break;
+                    case '*':
+                        this.stack.push(a * b)
+                        break;
+                    case '/':
+                        this.stack.push(a / b)
+                        break;
+                    case '^':
+                        this.stack.push(a ** b)
+                        break;
+                    case '%':
+                        this.stack.push((a / 100) * b)
+                        break;    
+                }
+            } else {
+                this.stack.push(i)
+            }
+        })
+        const result = this.stack.pop();
+        let resultStr = result.toString();
+        
+        if (resultStr.length > 12) {
+            let scientific = result.toExponential(8);
+            scientific = scientific.replace(/(\.\d*?[1-9])0+e/, '$1e');
+            this.currentOperand.innerText = scientific;
+        } else {
+            resultStr = resultStr.replace(/(\.\d*?[1-9])0+$|\.0+$/, '$1');
+            this.currentOperand.innerText = resultStr;
+        }    
     }
+
 
     isOperator(i){
         return this.operationList.includes(i)
     }
 
-    reversePOlNotationConv(){
+    isParenthesis(i){
+        return i === '(' || i === ')'
+    }
+
+    isHigherPrecedence(i, lastSymbol){
+        return this.precedence.get(i) > this.precedence.get(lastSymbol) || this.stack.length === 0 || lastSymbol === '('
+    }
+
+    reversePOlNotation(){
+        this.previousOperand.innerText = this.currentOperand.innerText + '=' 
         let expression = [...this.previousOperand.innerText]
         console.log(expression)
         let str = ''
         expression.forEach((i) => {
-            console.log(i)
              if(this.isOperator(i)){
                 let lastSymbol = this.stack.at(this.stack.length - 1)
                 this.output.push(parseInt(str))
                 str = ''
-                if(this.precedence.get(i) > this.precedence.get(lastSymbol) || this.stack.length === 0){
+                if(this.isHigherPrecedence(i, lastSymbol)){
                     this.stack.push(i)
-                } else {
+                }
+                else {
                     this.output.push(this.stack.pop())
                     this.stack.push(i)
                 }
-            } else  {
+            } else if(this.isParenthesis(i)){
+                if(i === '('){
+                    this.stack.push(i)
+                } else {
+                    if(str !== ''){
+                        this.output.push(parseInt(str))
+                        str = ''
+                    }                    
+                    while(this.stack.length > 0 && this.stack.at(this.stack.length - 1) !== '('){
+                        this.output.push(this.stack.pop())
+                    }
+                    this.stack.pop()
+                }
+            }
+            else  {
                 str += i;
             }
         })
-        this.output.push(parseInt(str))
-        this.output.push(this.stack)
-        console.log(this.stack)
+        if(str !== ''){
+            this.output.push(parseInt(str))
+        }
+        while(this.stack.length > 0){
+            this.output.push(this.stack.pop())
+        }   
         console.log(this.output)
-
     }
 
     clearAll() {
@@ -127,15 +208,6 @@ class Calculator {
     }
 
     appendNumber(number) {
-        for (let i = 0; i < this.operationList.length; i++) {
-            if (this.currentOperand.innerText === this.operationList[i]) {
-                this.previousOperand.innerText += this.currentOperand.innerText;
-                this.currentOperand.innerText = '';
-            }
-        }
-        // if (this.currentOperand.innerText === '0') {
-        //     this.currentOperand.innerText = '';
-        // }
         this.currentOperand.innerText += number;
     }
 
@@ -163,11 +235,8 @@ class Calculator {
     }
 
     chooseOperation(operation) {
-        this.previousOperand.innerText += this.currentOperand.innerText;
-        this.currentOperand.innerText = operation;
+        this.currentOperand.innerText += operation;
     }
-
-
 }
 
 const previousOperand = document.querySelector('.previous-operand');
